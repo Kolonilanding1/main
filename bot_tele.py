04344ce76484c8f7a8fcf17113d3ba34af3ed9b2
Koloni-Site <yunitia210514@gmail.com>
2025-07-10 23:12:57 +0700

    bot_tele.py

--- bot_tele.py
+++ bot_tele.py
@@ -9,7 +9,7 @@ from datetime import datetime
 
 auto_task = None
 
-API_TOKEN = '8110875332:AAG33TTd_CnmC_eFi58VhbNCSHL8A_EwfXA'
+TOKEN = os.getenv("BOT_TOKEN")
 GROUP_USERNAME = '@InfoFreebet4D','@SITUSLINKGACOR4D'
 CHANNEL_USERNAME = '@koloni4d_official1'
 TARGET_CHAT_ID = -1002658447462  # Ganti ini ke chat ID grup/channel tujuan untuk auto-text
